ba52d218c03caa13d8988d05d73e5b6ab135292f
mischkiey <mischkieycf@gmail.com>
2020-09-28 01:43:59 -0400

    Updated API endpoint

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export default {
   // Deployment automation script
   API_ENDPOINT: (process.env.REACT_APP_ENV === 'production')
-    ? 'https://glacial-falls-81805.herokuapp.com/api'
+    ? 'https://sheltered-stream-29634.herokuapp.com/api'
     : 'http://localhost:8000/api',
 
   TOKEN_KEY: process.env.REACT_APP_TOKEN_KEY || 'felix-felicis' // Temporary
